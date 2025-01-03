7db6d2c2a5c197b8ce25571b4bc706e355a63a9b
Tonmoy <tonmoysutradhar2002@gmail.com>
2025-01-03 21:04:33 +0600

    Oder section done with store the data in database. Hurrayyy

--- src/components/Modal/PurchaseModal.jsx
+++ src/components/Modal/PurchaseModal.jsx
@@ -5,60 +5,162 @@ import {
   TransitionChild,
   DialogPanel,
   DialogTitle,
-} from '@headlessui/react'
-import { Fragment } from 'react'
+} from "@headlessui/react";
+import { Fragment, useState } from "react";
+import Button from "../../components/Shared/Button/Button";
+import useAuth from "./../../hooks/useAuth";
 
-const PurchaseModal = ({ closeModal, isOpen }) => {
-  // Total Price Calculation
+import toast from "react-hot-toast";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+
+const PurchaseModal = ({ closeModal, isOpen, plant }) => {
+  const { name, price, image, seller, category, description, quantity, _id } =
+    plant;
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const [totalQuantity, setTotalQuantity] = useState();
+  const [totalPrice, setTotalPrice] = useState(price);
+  const [purchaseInfo, setPurchaseInfo] = useState({
+    customer: {
+      name: user?.displayName,
+      email: user?.email,
+      image: user?.photoURL,
+    },
+
+    plantID: _id,
+    price: totalPrice,
+    quantity: totalQuantity,
+    seller: seller?.email,
+    address: "",
+    status: "pending",
+  });
+
+  const handleQuantity = (value) => {
+    if (value > quantity) {
+      setTotalQuantity(quantity);
+      return toast.error("Quantity overloaded.");
+    }
+    if (value <= 0) {
+      setTotalQuantity(1);
+      return toast.error("Quantity can not be less than one.");
+    }
+    setTotalQuantity(value);
+    setTotalPrice(value * price);
+    setPurchaseInfo((prv) => {
+      return { ...prv, quantity: value, price: value * price };
+    });
+  };
+
+  // purchase
+  const handlePurchase = async () => {
+    // console.table(purchaseInfo);
+
+    // Data insert into DB
+    try {
+      axiosSecure.post("/order", purchaseInfo);
+      toast.success("Order successfully done.");
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <Transition appear show={isOpen} as={Fragment}>
-      <Dialog as='div' className='relative z-10' onClose={closeModal}>
+      <Dialog as="div" className="relative z-10" onClose={closeModal}>
         <TransitionChild
           as={Fragment}
-          enter='ease-out duration-300'
-          enterFrom='opacity-0'
-          enterTo='opacity-100'
-          leave='ease-in duration-200'
-          leaveFrom='opacity-100'
-          leaveTo='opacity-0'
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
         >
-          <div className='fixed inset-0 bg-black bg-opacity-25' />
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
         </TransitionChild>
 
-        <div className='fixed inset-0 overflow-y-auto'>
-          <div className='flex min-h-full items-center justify-center p-4 text-center'>
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
             <TransitionChild
               as={Fragment}
-              enter='ease-out duration-300'
-              enterFrom='opacity-0 scale-95'
-              enterTo='opacity-100 scale-100'
-              leave='ease-in duration-200'
-              leaveFrom='opacity-100 scale-100'
-              leaveTo='opacity-0 scale-95'
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
             >
-              <DialogPanel className='w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all'>
+              <DialogPanel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                 <DialogTitle
-                  as='h3'
-                  className='text-lg font-medium text-center leading-6 text-gray-900'
+                  as="h3"
+                  className="text-lg font-medium text-center leading-6 text-gray-900"
                 >
                   Review Info Before Purchase
                 </DialogTitle>
-                <div className='mt-2'>
-                  <p className='text-sm text-gray-500'>Plant: Money Plant</p>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">Plant: {name}</p>
                 </div>
-                <div className='mt-2'>
-                  <p className='text-sm text-gray-500'>Category: Indoor</p>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">Category: {category}</p>
                 </div>
-                <div className='mt-2'>
-                  <p className='text-sm text-gray-500'>Customer: PH</p>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">
+                    Customer: {user?.displayName}
+                  </p>
                 </div>
 
-                <div className='mt-2'>
-                  <p className='text-sm text-gray-500'>Price: $ 120</p>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">Price: $ {price}</p>
                 </div>
-                <div className='mt-2'>
-                  <p className='text-sm text-gray-500'>Available Quantity: 5</p>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">
+                    Available Quantity: {quantity}
+                  </p>
+                </div>
+
+                {/* Quantity  */}
+                <div className="space-x-1 text-sm my-2">
+                  <label htmlFor="quantity" className=" text-gray-600">
+                    Quantity:
+                  </label>
+                  <input
+                    // max={quantity}
+                    // value={totalQuantity}
+                    onChange={(e) => handleQuantity(parseInt(e.target.value))}
+                    className=" p-2 text-gray-800 border border-lime-300 focus:outline-lime-500 rounded-md bg-white"
+                    name="quantity"
+                    id="quantity"
+                    type="number"
+                    placeholder="Available quantity"
+                    required
+                  />
+                </div>
+
+                {/* Address */}
+                <div className="space-x-1 text-sm my-2">
+                  <label htmlFor="quantity" className=" text-gray-600">
+                    Address:
+                  </label>
+                  <input
+                    onChange={(e) => {
+                      setPurchaseInfo((prv) => {
+                        return { ...prv, address: e.target.value };
+                      });
+                    }}
+                    className=" p-2 text-gray-800 border border-lime-300 focus:outline-lime-500 rounded-md bg-white"
+                    name="address"
+                    id="address"
+                    type="text"
+                    placeholder="Your Address"
+                    required
+                  />
+                </div>
+
+                <div>
+                  <Button
+                    onClick={handlePurchase}
+                    label={`pay ${totalPrice}$ purchase`}
+                  ></Button>
                 </div>
               </DialogPanel>
             </TransitionChild>
@@ -66,7 +168,7 @@ const PurchaseModal = ({ closeModal, isOpen }) => {
         </div>
       </Dialog>
     </Transition>
-  )
-}
+  );
+};
 
-export default PurchaseModal
+export default PurchaseModal;
